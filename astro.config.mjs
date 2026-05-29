c361363515d8f484fcdc93789bef9422eee41646
Eliomar9523 <elioorh1001@gmail.com>
2026-05-29 17:13:20 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eliomar9523.github.io/Proyecto-Astro/',
+  site: 'https://eliomar9523.github.io',
+  base: '/Proyecto-Astro',
 });
 
 
